f74c05c7dbb3fc5adcb0c8d5329c432ef4f1dfdf
Edwin Corea Bonilla <edwin.corea@gmail.com>
2017-12-23 16:25:02 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+//Routes
+app.get("/api", (req, res) => {
+    res.json({
+        message: "Welcome to the API"
+    });
+});
+
+app.listen(5000, () => console.log("Server started on port 5000"));
